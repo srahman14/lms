79e93b504a6959045434c9b77a72b1ca81563eb8
Sayed <s.rahman14@hotmail.com>
2025-07-05 18:44:35 +0100

    excpetions

---
+++ models/exceptions.py
@@ -0,0 +1,11 @@
+class InvalidBookException(Exception):
+    # Exceptions for invalid ID input 
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class DuplicateIDException(Exception):
+    # Exception for when a dupliate ID is found
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
