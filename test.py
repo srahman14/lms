5436faa3d4caaeef2664624cf5571dcd0301e8db
Sayed <s.rahman14@hotmail.com>
2025-07-06 04:50:27 +0100

    tests

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from models.book import Book
 from library_operations import add_book, list_all_books
+from storage import load_books, save_books
+from models.catalog import Catalog
 
-# add_book(isbn=1234567890, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
-list_all_books()
+cat = Catalog()
+cat.add_book(isbn=1234567890, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
+
+# list_all_books()
