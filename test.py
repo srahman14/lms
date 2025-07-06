04e97adf55fcfe39901b66e00e8b227ac325cf50
Sayed <s.rahman14@hotmail.com>
2025-07-06 18:13:28 +0100

    tests

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ from storage import load_books, save_books
 from models.catalog import Catalog
 
 cat = Catalog()
-cat.add_book(isbn=1234567890, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
-
+# cat.add_book(isbn=1234567890, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
+# cat.add_book(isbn=1234567891, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
+# cat.add_book(isbn=1234567892, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
 # list_all_books()
+
+# cat.remove_book(isbn=1234567890)
+cat.remove_book(isbn=1234567891)
+# cat.view_books()
