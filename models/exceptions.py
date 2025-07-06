dc0f363e42d71aaa470fdf96020bbfbe48be9434
Sayed <s.rahman14@hotmail.com>
2025-07-06 04:50:18 +0100

    added invalid ISBN exception

--- models/exceptions.py
+++ models/exceptions.py
@@ -4,6 +4,12 @@ class InvalidBookException(Exception):
         self.message = message
         super().__init__(self.message)
 
+class InvalidISBN(Exception):
+    # Exceptions for invalid ID input 
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 class DuplicateIDException(Exception):
     # Exception for when a dupliate ID is found
     def __init__(self, message):
