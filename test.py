45c6de5624a1cdacb69abfcdd7e6ab06e43041ee
Sayed <s.rahman14@hotmail.com>
2025-08-10 17:16:16 +0100

    tests

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from storage import load_books, save_books
 from models.catalog import Catalog
 
 cat = Catalog()
-# cat.add_book(isbn=1234567890, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
-# cat.add_book(isbn=1234567891, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
+# cat.add_book(isbn=1234567890, title="Harry Potter", author="J.K Rowling", publisher="Owl", pub_year=2005, genre="Sci-fi", available=15, total=20)
+# cat.add_book(isbn=1234567891, title="Atomic Habits", author="James Clear", publisher="Fox", pub_year=2002, genre="Productivity", available=10, total=20)
 # cat.add_book(isbn=1234567892, title="Test", author="Test Author", publisher="Test Pub", pub_year=2005, genre="Test Genre", available=10, total=20)
 # list_all_books()
 
@@ -13,4 +13,5 @@ cat = Catalog()
 # cat.remove_book(isbn=1234567891)
 # cat.view_books()
 # cat.update_book(1234567892, field="Author", new_val="Test Update Author")
-cat.check_availablity(1234567892)
+# cat.check_availablity(1234567891)
+# cat.search_books(isbn=None, title="Test", author="James Clear", genre=None)
